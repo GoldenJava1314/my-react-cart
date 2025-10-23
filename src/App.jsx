744f7d3f133f158ebd1883bb426c22b8e80beab1
GoldenJava1314 <just4269@gmail.com>
2025-10-23 19:45:45 +0800

    配置版面

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,27 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Route, Router } from 'react-router-dom'
+import Navbar from "./components/Navbar"
+import Home from "./pages/Home"
+import Footer from "./components/Footer"
 
 function App() {
 
   return (
-    <>
-      Main Page
-    </>
+    <Router>
+      {/*導航列-位於最上方*/}
+       <Navbar />
+
+       
+
+      {/*主要內容區-位於中間部分*/}
+       <Home />
+
+      {/*頁尾-位於最下方*/ }
+       <Footer />
+
+    </Router>
   )
 }
 
